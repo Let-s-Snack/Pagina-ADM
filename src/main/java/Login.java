cc89d48cb5d56fa2734ded24dc38e9ca2fdc6dc1
Beatriz <b.tmarioti3.0@gmail.com>
2024-10-01 06:39:59 -0300

    subindo adm page

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,7 +1,9 @@
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -17,11 +19,15 @@ public class Login extends HttpServlet {
             pstmt.setString(1,email);
             pstmt.setString(2,password);
             if (pstmt.execute()){
-                req.getRequestDispatcher("resultado.jsp").forward(req,resp);
+                req.getRequestDispatcher("indexAdm.html").forward(req,resp);
             }
 
         }catch (SQLException se){
             se.printStackTrace();
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
